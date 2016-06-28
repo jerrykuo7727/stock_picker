02b3c126e5280921a9a2bbf6e027bea3f8a2603f
jerrykuo7727 <jerrykuo7727@gmail.com>
2016-06-28 23:47:16 +0800

    Finish the stock picker

---
+++ stock_picker.rb
@@ -0,0 +1,23 @@
+def stock_picker(prices)
+	size = prices.size
+	profit = 0
+	days = [-1, -1]
+	(0...size-1).each do |i|
+		(i+1...size).each do |j|
+			if prices[j] - prices[i] > profit
+				profit = prices[j] - prices[i]
+				days = [i, j]
+			end
+		end
+	end
+	return days
+end
+
+puts "*********************************************"
+puts "* Please enter the stock prices at each day *"
+puts "*  to get the best timing to sell and buy.  *"
+puts "*********************************************"
+puts "Please enter the prices: "
+stock_prices = gets.split.map(&:to_i)
+timing = stock_picker(stock_prices)
+puts "Buy at day #{timing[0]+1}, sell at day #{timing[1]+1}."
